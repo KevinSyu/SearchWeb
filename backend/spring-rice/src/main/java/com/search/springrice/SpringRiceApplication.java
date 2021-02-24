26e3c3e6f18dc80d14ae33c3d5b49ee485b9e5e0
Kevin <l8iunix8@gmail.com>
2021-02-24 21:52:40 +0800

    2021/02/24 project init

---
+++ backend/spring-rice/src/main/java/com/search/springrice/SpringRiceApplication.java
@@ -0,0 +1,13 @@
+package com.search.springrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRiceApplication.class, args);
+	}
+
+}
---
+++ backend/spring-rice/src/test/java/com/search/springrice/SpringRiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.search.springrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
