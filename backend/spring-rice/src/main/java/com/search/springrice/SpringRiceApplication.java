d7665b6519a716b481c5b64b0bc29b902258541b
Kevin <l8iunix8@gmail.com>
2021-02-27 16:33:50 +0800

    2021/02/27 add controller and filter  CORS Policy

--- backend/spring-rice/src/main/java/com/search/springrice/SpringRiceApplication.java
+++ backend/spring-rice/src/main/java/com/search/springrice/SpringRiceApplication.java
@@ -2,6 +2,7 @@ package com.search.springrice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SpringRiceApplication {
@@ -9,5 +10,5 @@ public class SpringRiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRiceApplication.class, args);
 	}
-
+	
 }
